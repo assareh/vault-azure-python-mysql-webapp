d855ceb836e27fd8ed487273b0e062ead15b0683
Andy Assareh <assareh@hashicorp.com>
2020-08-24 16:02:52 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,3 @@
 terraform {
   required_version = ">=0.12"
-
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "multicloud-provisioning-demo"
-    workspaces {
-      name = "vault-azure-python-mysql-webapp"
-    }
-  }
 }
